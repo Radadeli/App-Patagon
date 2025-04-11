85b87bafaaf3a20c010293f295ed3f91e7ef2b76
Ramiro De Lillo <ramirodelillo@gmail.com>
2025-04-11 14:34:08 +0200

    fix images names

--- src/interfaces/ui/pages/WhyUs/Adventures.tsx
+++ src/interfaces/ui/pages/WhyUs/Adventures.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./adventures.module.scss";
-import adventure from "/src/assets/images/Adventure.png";
+import adventure from "/src/assets/images/adventure.png";
 
 const Adventures: React.FC = () => {
   return (
